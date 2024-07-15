b731d4a43fbf7ac907f3898e526897b8154402c9
sukimixcr <sukimixcr@gmail.com>
2024-07-15 12:53:39 -0700

    registration form displaying , not working yet

---
+++ capstone-prep-client/src/app/api.js
@@ -0,0 +1,55 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createSlice } from "@reduxjs/toolkit";
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://capstone-prep-backend-vjwd.onrender.com/",
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().registration.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["User"],
+  endpoints: (builder) => ({
+    registration: builder.mutation({
+      query: (credentials) => ({
+        url: "/api/user/register",
+        method: "POST",
+        body: credentials,
+      }),
+      invalidateTags: ["User"],
+    }),
+  }),
+});
+
+const registrationSlice = createSlice({
+  name: "registration",
+  initialState: {},
+  reducers: {
+    setToken: ({ payload }) => {
+      window.sessionStorage.setItem("Token", payload.token);
+    },
+
+    clearToken: () => {
+      window.sessionStorage.removeItem("Token");
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(api.endpoints.registration.matchFulfilled, setToken);
+  },
+});
+
+// const storeToken = (state, { payload }) => {
+//   state.token = payload.token;
+//   state.message = payload.message;
+// };
+
+export const { useRegistrationMutation } = api;
+
+export const { setToken, clearToken } = registrationSlice.actions;
+
+export default registrationSlice.reducer;
--- capstone-prep-client/src/components/Registration.jsx
+++ capstone-prep-client/src/components/Registration.jsx
@@ -1,1 +1,104 @@
-// 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRegistrationMutation } from "./../app/api";
+
+export default function Registration() {
+  const [form, setForm] = useState({});
+  const [errM, setErrM] = useState(null);
+  const navigate = useNavigate();
+  const [registerUser] = useRegistrationMutation();
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    try {
+      let success = false;
+
+      success = await registerUser(form).unwrap();
+
+      console.log("sux es" + success);
+
+      if (success) {
+        window.sessionStorage.setItem("Token", success.token);
+        navigate("/");
+      }
+    } catch (err) {
+      setErrM(err.data.message);
+    }
+  };
+
+  const updateForm = (e) => {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  return (
+    <>
+      <div className="space"></div>
+
+      <div className="contenedor formulario_solo center">
+        <div className="hotpink register">
+          <p className="hotpink playwrite">Register</p>
+
+          <form onSubmit={submit} name="formRegister">
+            <div className="form-group">
+              <label></label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                name="email"
+                onChange={updateForm}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label></label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                name="password"
+                onChange={updateForm}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label></label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First Name"
+                name="firstName"
+                onChange={updateForm}
+              />
+            </div>
+
+            <div className="form-group">
+              <label></label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Last Name"
+                name="lastName"
+                onChange={updateForm}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Register
+            </button>
+          </form>
+          {errM && (
+            <div>
+              <p className="space"></p>
+              <p className="error">{errM}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
