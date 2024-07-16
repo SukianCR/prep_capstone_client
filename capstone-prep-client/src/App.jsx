1ede94ded5cab50acd8a15d38c4e1fd6b145a284
Drew Hastings <drewhastings@Drews-MacBook-Pro.local>
2024-07-16 11:59:09 -0400

    added protected route

--- capstone-prep-client/src/App.jsx
+++ capstone-prep-client/src/App.jsx
@@ -4,12 +4,16 @@ import Login from "./components/Login.jsx";
 import Registration from "./components/Registration";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
+import Protected from "./components/Protected.jsx";
 
 function App() {
   return (
     <Provider store={store}>
       <div>
         <Routes>
+          <Route path="/" element={<Protected />}>
+            <Route path="/" element={<Home />}></Route>
+          </Route>
           <Route path="/registration" element={<Registration />}></Route>
           <Route path="/login" element={<Login />}></Route>
         </Routes>
